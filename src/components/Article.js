402ac249b8dc87d88bc3b19fb80f40edc69dd0ee
HanadAlly <allyhanad404@gmail.com>
2025-04-14 15:30:08 +0300

    commit changes

---
+++ src/components/Article.js
@@ -0,0 +1,23 @@
+  import React from "react";
+ 
+  function Article({ title,date ="January 1, 1970", preview,minutes}) {
+   const renderEmojis = ( ) => {
+     if (minutes < 30) {
+       return "☕️".repeat(Math.ceil(minutes / 5));
+     } else {
+     return "🍽️".repeat((minutes / 10 ));
+ }
+   };
+ 
+     return (
+         <article>
+         <h3>{title}</h3>
+         <small>
+        {date} {renderEmojis()} {minutes} min read 
+         </small>
+         <p>{preview}</p>
+         </article>
+     );
+ }
+ 
+export default Article;
